9a1968c71b3cbea37136ee689e3f3d50ed084e9e
tomo1405 <tomo20020510@icloud.com>
2026-03-19 18:37:01 +0900

    change font-family

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772974224',resources: [
+    cacheName: 'chirpy-1773912813',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
