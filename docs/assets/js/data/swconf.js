35596b80ed2d63bf8be52fe9f3bca973843d7493
tomo1405 <tomo20020510@icloud.com>
2026-04-06 20:58:49 +0900

    new post

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775476615',resources: [
+    cacheName: 'chirpy-1775476712',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
